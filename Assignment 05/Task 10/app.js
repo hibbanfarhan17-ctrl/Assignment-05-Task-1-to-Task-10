2130d058634d630a1644c1da9f4105f50eb6186a
hibbanfarhan17-ctrl <hibbanfarhan.17@gmail.com>
2026-06-28 23:02:37 +0500

    Delete Assignment 05/Task 10 directory

--- Assignment 05/Task 10/app.js
+++
@@ -1,6 +0,0 @@
-// Range Counter
-var startNum = prompt("Enter the first number")
-var endNum = prompt("Enter the last number")
-for (let i = startNum; i <=endNum; i++) {
-    document.write(i + '<br>')
-}
